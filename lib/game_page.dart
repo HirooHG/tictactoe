e84f76d01483eea7f769c786587360703f0907bc
HirooHG <hugo.golliet@etu.univ-smb.fr>
2022-07-05 12:49:31 +0200

    win condition changes

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 import 'game_communication.dart';
 
+Future<void> popup({String? title = "GG !", String? text = "", required BuildContext context}) async{
+
+  await showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title!),
+        content: Text(
+          "$text won !" ,
+          style: const TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontSize: 30
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
 class GamePage extends StatefulWidget {
   const GamePage({
     Key? key,
@@ -62,6 +91,20 @@ class _GamePageState extends State<GamePage> {
     game.send('resign', '');
     Navigator.pop(context);
   }
+  bool _verifGrid(){
+    bool x1 = grid[0] == widget.character && grid[1] == widget.character && grid[2] == widget.character;
+    bool x2 = grid[3] == widget.character && grid[4] == widget.character && grid[5] == widget.character;
+    bool x3 = grid[6] == widget.character && grid[7] == widget.character && grid[8] == widget.character;
+
+    bool y1 = grid[0] == widget.character && grid[3] == widget.character && grid[6] == widget.character;
+    bool y2 = grid[1] == widget.character && grid[4] == widget.character && grid[7] == widget.character;
+    bool y3 = grid[2] == widget.character && grid[5] == widget.character && grid[8] == widget.character;
+
+    bool d1 = grid[0] == widget.character && grid[4] == widget.character && grid[8] == widget.character;
+    bool d2 = grid[2] == widget.character && grid[4] == widget.character && grid[6] == widget.character;
+
+    return x1 || x2 || x3 || y1 || y2 || y3 || d1 || d2;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,6 +153,11 @@ class _GamePageState extends State<GamePage> {
 
           turn = false;
           setState((){});
+        }
+
+        if(_verifGrid()){
+          popup(context: context, text: game.playerName);
+          Navigator.pop(context);
         }
       },
       child: GridTile(
